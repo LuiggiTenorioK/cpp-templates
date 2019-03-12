6b614b81e75a9fd3d86a5dfb8ccc3770bed2d68b
Luiggi Gianpiere Tenorio Ku <luiggi.tenorio@pucp.pe>
2019-03-12 14:37:30 -0300

    12-03-19 update 2

--- Math/primes.cpp
+++ Math/primes.cpp
@@ -32,6 +32,7 @@ ll _sieve_size;
 bitset<10000010> bs;   // 10^7 should be enough for most cases
 vi primes;
 
+// SIEVE
 
 void sieve(ll upperbound) {
 	_sieve_size = upperbound + 1; // add 1 to include upperbound
@@ -50,6 +51,19 @@ bool isPrime(ll N) {
 	for (int i = 0; i < (int)primes.size(); i++)
 		if (N % primes[i] == 0) return false;
 	return true;
+}
+
+// FACTORS
+
+vi primeFactors(ll N) {
+	vi factors;
+	ll PF_idx = 0, PF = primes[PF_idx];
+	while (N != 1 && (PF * PF <= N)) {
+		while (N % PF == 0) { N /= PF; factors.push_back(PF); }
+		PF = primes[++PF_idx]; 
+	}
+	if (N != 1) factors.push_back(N);
+	return factors;
 }
 
 int main(){
