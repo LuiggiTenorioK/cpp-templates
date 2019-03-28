6f0a251c10c8abf0a1619160b9e5962affef0203
LuiggiTenorioK <luiggi.tenorio@pucp.pe>
2019-03-27 22:24:08 -0500

    unionFind

---
+++ Graph/unionFind.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int p[100000];
+ 
+int find(int u){
+    return (p[u] == u) ? u:(p[u]=find(p[u]));
+}
+ 
+void unionFind(int u, int v){
+    int pu=find(u), pv=find(v);
+    if(pu!=pv) p[pu]=pv;
+}
+ 
+int main(){
+    //el codigo segun la situacion varia aqui
+    return 0;
+}
